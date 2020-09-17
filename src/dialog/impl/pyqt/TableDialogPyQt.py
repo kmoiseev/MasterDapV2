72a48a0ae4d46a2d7726b0cbbc417fa49605b173
andreyzaytsev21 <62799231+andreyzaytsev21@users.noreply.github.com>
2020-09-17 20:24:26 +0300

    17.09 table pyqt

--- src/dialog/impl/pyqt/TableDialogPyQt.py
+++ src/dialog/impl/pyqt/TableDialogPyQt.py
@@ -1,9 +1,17 @@
+import sys
+
+from PyQt5 import Qt
+from PyQt5.QtGui import QPalette, QColor
+
 from abc import ABC
-from tkinter import Tk
+
+from PyQt5.QtWidgets import QWidget
 
 from src.dialog.common.table.TableDialog import TableDialog
 from src.dialog.common.table.TableFuncs import TableFuncs
 from src.dialog.common.table.data.Table import Table
+from src.template.TemplateManager import TemplateManager
+from src.template.table.TableTemplate import TableTemplate
 
 
 class TableDialogPyQt(TableDialog, ABC):
@@ -11,13 +19,34 @@ class TableDialogPyQt(TableDialog, ABC):
     def __init__(self, table_funcs: TableFuncs):
         super().__init__(table_funcs)
 
-        self.root = Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.funcs.closed_on_x)
-
     def show(self):
-        self.root.title("Table")
         table: Table = self.funcs.get_table_data()
-        self.root.mainloop()
+
+        class ManageDealsWidget(QWidget):
+
+            def __init__(self):
+                super().__init__()
+                layout = Qt.QVBoxLayout(self)
+                labels = list(map(lambda cell: cell.value, table.header.cells))
+                table_widget = Qt.QTableWidget(0, len(labels))
+                table_widget.setColumnHidden(len(labels), False)
+                table_widget.setHorizontalHeaderLabels(labels)
+                for k, row in enumerate(table.rows):
+                    table_widget.insertRow(table_widget.rowCount())
+                    for j, val in enumerate(list(map(lambda cell: cell.value, row.cells))):
+                        table_widget.setItem(k, j, Qt.QTableWidgetItem(val))
+                        table_widget.resizeColumnsToContents()
+                table_widget.show()
+                layout.addWidget(table_widget)
+
+        app = Qt.QApplication([])
+        manage_deals_widget = ManageDealsWidget()
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor('#bdf0d4'))
+        manage_deals_widget.setPalette(palette)
+        manage_deals_widget.resize(900, 600)
+        manage_deals_widget.show()
+        app.exec()
 
     def close(self):
-        self.root.destroy()
+        sys.exit()
